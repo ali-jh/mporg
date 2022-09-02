aa09b503c3d9d466aa708387fa29170ea2ca3fba
ali-jh <alijh9518@yahoo.com>
2022-09-02 12:26:37 +0430

    create new hook for use local storage// create style module for login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import useLocalStorage from '../hooks/useLocalStorage';
+import styles from "./Login.module.css";
 
 const Login = () => {
+    const [name,setName] = useLocalStorage("name", "")
+    const [pass,setPass] = useLocalStorage("pass", "")
+
     return (
-        <div>
-            <h1>Loading</h1>
+        <div className={styles.loginform}>
+            <form>
+                <h1>Login</h1>
+                <div className={styles.content}>
+                    <div className={styles.inputfield}>
+                    <input type="text" value={name} onChange={event => setName(event.target.    value)} placeholder="Email" autoComplete='nope'/>
+                    </div>
+                    <div className={styles.inputfield}>
+                    <input type="password" value={pass} onChange={event => setPass(event.target. value)} placeholder="password" autoComplete='nope'/>
+                    </div>
+                </div>
+            </form>
         </div>
+
+        
     );
 };
 
